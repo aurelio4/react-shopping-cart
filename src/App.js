f6cf62b7d7c75ebf126cef664ab1b230e6e7782a
chelkuhs <aurelioarcabascio@gmail.com>
2020-04-16 16:37:02 -0400

    finished project + stretch

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import data from './data';
 import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
+import { ProductContext } from './contexts/ProductContext';
+import { CartContext } from './contexts/CartContext';
 
 function App() {
 	const [products] = useState(data);
@@ -13,20 +15,32 @@ function App() {
 
 	const addItem = item => {
 		// add the given item to the cart
+		setCart([...cart, item])
 	};
+
+	const removeItem = item => {
+		const itemToBeRemoved = cart.find(id => item === id.id)
+		const cartCopy = [...cart]
+		const newCart = cartCopy.indexOf(itemToBeRemoved) === 0 ? cartCopy.splice(cartCopy.indexOf(itemToBeRemoved), 1) : cartCopy.splice(cartCopy.indexOf(itemToBeRemoved), cartCopy.indexOf(itemToBeRemoved))
+		setCart(cartCopy)
+	}
 
 	return (
 		<div className="App">
 			<Navigation cart={cart} />
 
-			{/* Routes */}
-			<Route exact path="/">
-				<Products products={products} addItem={addItem} />
-			</Route>
+			<ProductContext.Provider value={{ products, addItem }}>
+				<CartContext.Provider value={{ cart, removeItem }}>
+					{/* Routes */}
+					<Route exact path="/">
+						<Products />
+					</Route>
 
-			<Route path="/cart">
-				<ShoppingCart cart={cart} />
-			</Route>
+					<Route path="/cart">
+						<ShoppingCart />
+					</Route>
+				</CartContext.Provider>
+			</ProductContext.Provider>
 		</div>
 	);
 }
